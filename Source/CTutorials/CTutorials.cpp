1ed2619f272a68d3200476ac22171c990383f55a
Olly Rippon <ripponolly@gmail.com>
2019-07-22 13:27:07 +0100

    Initial commit

---
+++ Source/CTutorials/CTutorials.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CTutorials.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CTutorials, "CTutorials" );
